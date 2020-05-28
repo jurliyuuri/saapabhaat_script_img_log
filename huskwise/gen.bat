d46a5f3b697e91fe61c4534ed81269b5e7bdbf51
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2020-05-28 23:37:16 +0900

    手動で切り分けimagemagickで分解

---
+++ huskwise/gen.bat
@@ -0,0 +1,12 @@
+for /l %%x in (1, 1, 22) do (
+    magick %%x.png -fill black +opaque #ff00f2 a\%%xÉ}.png
+    magick %%x.png -fill black +opaque #ed1c24 a\%%xê‘.png
+    magick %%x.png -fill black +opaque #ff7f27 a\%%xûÚ.png
+    magick %%x.png -fill black +opaque #fff200 a\%%xâ©.png
+    magick %%x.png -fill black +opaque #22b14c a\%%xóŒ.png
+    magick %%x.png -fill black +opaque #00a2e8 a\%%xêÖ.png
+    magick %%x.png -fill black +opaque #3f48cc a\%%xê¬.png
+    magick %%x.png -fill black +opaque #a349a4 a\%%xéá.png
+    magick %%x.png -fill black +opaque #880015 a\%%xíÉ.png
+    magick %%x.png -fill black +opaque #ffaec9 a\%%xìç.png
+)
